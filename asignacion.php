4e34637a38fec7900e35a2c2863fd166353f10b7
Ulises <uli05112001@gmail.com>
2025-08-04 19:59:38 -0600

    Ej de asignacion

---
+++ asignacion.php
@@ -0,0 +1,16 @@
+<?php 
+// Operadores de asignacion rester $variable += y el numero lo mismo con todas las demas 
+// suma, multiplicaicon, divicion, concatenacion:
+
+$num=10;
+$num-=5;
+$num+=5;
+$num/=2;
+$num*=5;
+$num-=4;
+
+echo "perry trae el numero : ".$num;
+
+
+
+?>
