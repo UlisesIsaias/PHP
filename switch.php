479e963d1182a2a3207efca2d471c410af95594c
Ulises <uli05112001@gmail.com>
2025-08-05 22:44:17 -0600

    new switch

--- switch.php
+++ switch.php
@@ -1,5 +1,7 @@
 <?php
 
+//Mostrar la fruta que sea o si no es una fruta.
+
 $fruta="fresa";
 
 switch($fruta){
@@ -15,6 +17,38 @@ switch($fruta){
     default:
     echo "no eres una fruta";
 }
+
+
+
+// Los dias de la semana 
+
+$dia=7;
+
+switch($dia){
+    case "1":
+        echo "lunes";
+        break;
+    case "2":
+        echo " martes";
+        break;
+    case "3";
+        echo "Miercoles";
+        break;
+    case "4";
+        echo "jueves";
+        break;
+    case "5";
+        echo "Viernes";
+        break;
+    case "6";
+        echo "Sabado";
+        break;
+    case "7" :
+        echo "Domingo";
+        break;
+    default:
+    echo "no eres una dia";
+} 
 
 
 ?>
