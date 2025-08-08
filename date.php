14bbdd8004215a4a39a2910b288873933ecebf11
Ulises <uli05112001@gmail.com>
2025-08-07 23:08:57 -0600

    dates

--- date.php
+++ date.php
@@ -1,9 +1,37 @@
 <?php
 
+
+// Obtener fecha y hora.
 date_default_timezone_set("America/Monterrey");
 
-echo date("d/m/y");
+$fecha_us=date("l d f Y");
+$fecha_us=date("d/m/Y");
 
+
+$h_12=date("H:i" );
+
+$fecha_completa=date("d/m/Y - h:i a");
+//echo "la fecha completa es ".$fecha_completa;
+
+
+//  Fecha en español
+
+function fecha_es(){
+    $fecha_dia=date("d");
+    $fecha_mes=date("m");
+    $fecha_year=date("Y");
+
+    $dia_sema = [
+    "Monday"    => "lunes",
+    "Tuesday"   => "martes",
+    "Wednesday" => "miércoles",
+    "Thursday"  => "jueves",
+    "Friday"    => "viernes",
+    "Saturday"  => "sábado",
+    "Sunday"    => "domingo"
+    ];
+};
+echo fecha_es();
 
 
 
